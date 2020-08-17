844e15cec168040869a4dfdab20985ba1325cfae
Namra Abid <s.namraabid@gmail.com>
2020-08-17 20:18:09 +0500

    some changes in libarry.py

--- library.py
+++ library.py
@@ -19,4 +19,29 @@ class Library:
    def returnBook(self,book):
        self.lendDict.pop(book)
 FirstLibrary=Library(['Python','JKP','KKMT','Fault in Our Stars','Harry Potter'],'ALibraryBy_NamraAbid')
-print(FirstLibrary.booklist,"\n",FirstLibrary.name)
+#print(FirstLibrary.booklist,"\n",FirstLibrary.name)
+while(True):
+    print(f"Welcome to {FirstLibrary.name} ")
+    print("Enter your choice to continue")
+    print("1.Display Book")
+    print("1.Lend a Book")
+    print("1.Add a Book")
+    print("1.Return a Book")
+    user_choice = int(input())
+    if user_choice==1:
+        harry.displayBook()
+    elif user_choice==2:
+        pass
+    elif user_choice==3:
+        pass
+    elif user_choice==4:
+        pass
+    else:
+        print("Not a valid option")
+    print("press Q to quit and C to Continue")
+    
+    user_choice2=input()
+    if user_choice2=='q':
+        exit()
+    if user_choice2=="c"
+        continue
